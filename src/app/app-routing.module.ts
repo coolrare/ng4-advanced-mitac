616f8f1f6e9c58ea2d3b3634f6f1ee279e53f926
Will保哥 <doggy.huang@gmail.com>
2017-05-24 14:51:21 +0800

    示範從路由定義設定 data 屬性

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { fallbackRoute } from 'app/shared/fallback-route';
 
 const routes: Routes = [
   { path: '', redirectTo: 'page1', pathMatch: 'full' },
-  { path: 'page1', component: Page1Component },
+  { path: 'page1', component: Page1Component, data: { title: 'This is Page 1' } },
   { path: 'page2/sub1/sub2', component: Page2Component },
   fallbackRoute
 ];
