c4153e817603d8c2f2e7b8ca067da74a23a6f249
Will保哥 <doggy.huang@gmail.com>
2017-05-24 14:37:31 +0800

    獨立某一條路由定義

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Route } from '@angular/router';
 import { Page1Component } from 'app/page1/page1.component';
 import { Page2Component } from 'app/page2/page2.component';
+
+const fallbackRoute: Route = {
+  path: '**', redirectTo: '/page1', pathMatch: 'full'
+};
 
 const routes: Routes = [
   { path: '', redirectTo: 'page1', pathMatch: 'full' },
   { path: 'page1', component: Page1Component },
   { path: 'page2/sub1/sub2', component: Page2Component },
-  { path: '**', redirectTo: '/page1', pathMatch: 'full' }
+  fallbackRoute
 ];
 
 @NgModule({
