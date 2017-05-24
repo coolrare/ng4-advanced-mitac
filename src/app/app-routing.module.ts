733e293fb41b2218b57950b3015aeaac44f617ad
Will保哥 <doggy.huang@gmail.com>
2017-05-24 15:07:12 +0800

    建立 dashboard 元件並加入路由定義

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Route } from '@angular/router';
 import { Page1Component } from 'app/page1/page1.component';
@@ -5,7 +6,8 @@ import { Page2Component } from 'app/page2/page2.component';
 import { fallbackRoute } from 'app/shared/fallback-route';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'page1', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: Page1Component, data: { title: 'This is Page 1' } },
   { path: 'page2/sub1/sub2', component: Page2Component },
   fallbackRoute
