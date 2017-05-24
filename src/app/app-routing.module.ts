f8a3a46bfb9522e7275a66b7ca3a5a0b60b940c7
Will保哥 <doggy.huang@gmail.com>
2017-05-24 14:13:58 +0800

    示範預設路由定義

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Page1Component } from 'app/page1/page1.component';
 import { Page2Component } from 'app/page2/page2.component';
 
 const routes: Routes = [
+  { path: '', component: Page1Component },
   { path: 'page1', component: Page1Component },
   { path: 'page2/sub1/sub2', component: Page2Component }
 ];
