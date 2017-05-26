54e0646411be5339336b2fcd542ebc6dbdabeb1e
Will保哥 <doggy.huang@gmail.com>
2017-05-26 10:01:55 +0800

    建立 ChartsModule 並將 FlotComponent 移至 ChartsModule 裡

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,11 @@ import { Page2Component } from 'app/page2/page2.component';
 import { fallbackRoute } from 'app/shared/fallback-route';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
-import { FlotComponent } from './charts/flot/flot.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'cards/:type', component: CardsComponent },
-  { path: 'charts', children: [
-      { path: 'flot', component: FlotComponent }
-  ]},
   { path: 'page1', component: Page1Component, data: { title: 'This is Page 1' } },
   { path: 'page2/sub1/sub2', component: Page2Component },
   fallbackRoute
