b1f85f89049b281abb785d0993c3a152db4e8185
Will保哥 <doggy.huang@gmail.com>
2017-05-26 15:47:49 +0800

    匯入 ReactiveFormsModule 模組

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -28,6 +28,7 @@ import { ClassicComponent } from './forms/classic/classic.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule
   ],
